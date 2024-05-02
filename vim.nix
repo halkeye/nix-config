71902eec5e12bc3989254eb3af87ee7ac7e9b545
Gavin Mogan <git@gavinmogan.com>
2024-05-01 23:49:10 -0700

    stuff

--- vim.nix
+++ vim.nix
@@ -18,6 +18,7 @@ in
   vimdiffAlias = true;
   defaultEditor = true;
   plugins = with pkgs.vimPlugins; [
+    nvim-treesitter
     editorconfig-vim
     fzf-vim
     fzfWrapper
@@ -28,6 +29,7 @@ in
     vim-better-whitespace
     vim-multiple-cursors
     vim-surround
+    copilot-vim
 
     # :AG
     ag-nvim # rking/ag.vim
@@ -41,19 +43,30 @@ in
     wombat256
     papercolor-theme
 
-    { plugin = vim-polyglot; config = ''let g:polyglot_disabled = ['md', 'markdown']''; }
-    vim-go
-    vim-nix
-
     vim-commentary # commentary.vim: comment stuff out
     vim-eunuch # :Remove, :Rename, etc
 
     vim-trailing-whitespace
 
+    trouble-nvim # show errors
     nvim-lspconfig
 
-#    (fromGitHub "728374ef59b11a5f5991ea2560d149a4ae33fd22" "master" "yasuhiroki/github-actions-yaml.vim")
-    #(fromGitHub "e7acbbbfde3f09a378873a16a380f8fdb01a4d12" "v0.1.17" "tekumara/typos-lsp")
+    # Completion
+    nvim-cmp
+    cmp-copilot
+    cmp-dictionary
+    cmp-emoji
+    cmp-nvim-lsp
+    cmp-nvim-ultisnips
+    cmp-path
+    cmp-treesitter
+
+    # Languages
+    markdown-preview-nvim
+    typescript-tools-nvim
+    vim-go
+    vim-nix
+    (fromGitHub "e7acbbbfde3f09a378873a16a380f8fdb01a4d12" "main" "tekumara/typos-lsp")
   ];
 
   coc = {
