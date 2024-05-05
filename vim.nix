cefd1d8308993a6a7accb53ec7102682d56f1002
Gavin Mogan <git@gavinmogan.com>
2024-05-05 10:13:32 -0700

    vim telescope

--- vim.nix
+++ vim.nix
@@ -55,6 +55,8 @@ in
     trouble-nvim
     lightline-lsp
     lsp-format-nvim
+    telescope-nvim
+    telescope-fzy-native-nvim
 
     # Completion
     nvim-cmp
@@ -126,6 +128,12 @@ in
     require'lspconfig'.ruby_lsp.setup{}
     require'lspconfig'.gopls.setup{}
     require'lspconfig'.nixd.setup{}
+
+    local telescopeBuiltin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', telescopeBuiltin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', telescopeBuiltin.help_tags, {})
   '';
   extraPackages = with pkgs; [
     rust-analyzer
